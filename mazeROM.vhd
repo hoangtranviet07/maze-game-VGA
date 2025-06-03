9d51ad3f3f7aecd7504c852f40e54ab15b5c45b4
Collin Harrington <collin1234harr@gmail.com>
2025-06-03 16:22:07 -0400

    Basic Structure Added

--- mazeROM.vhd
+++ mazeROM.vhd
@@ -9,25 +9,25 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity mazeROM is 
-	generic(
-		NUM_MAZES : integer := 1;
-		SIZE : integer := 8
+	-- Deleted the Generics, we can worry about it at the end if we have time
+	port(   
+		read_addr : input std_logic_vector(3 downto 0);
+		maze_out  : output std_logic_vector(75 downto 0)
 	);
-	port( RA : input std_logic_vector(NUM_MAZES - 1 downto 0); -- Fix this later
-			Maze : output std_logic_vector(SIZE * SIZE - 1 downto 0);
-			Goal : output std_logic_vector(5 downto 0) --Fix this 
 end entity;
 
 architecture mazeRom_arch of mazeROM is 
 
-	type Memory : array (NUM_MAZES-1 downto 0) of std_logic_vector(SIZE * SIZE - 1 + 6 downto 0);
+	type Memory : array (3 downto 0) of std_logic_vector(75 downto 0);
 
-	constant Mazes : Memory := (); --Put a maze here
+	constant Mazes : Memory := (); --Put the mazes here
 
 	signal addr_num : integer;
 	
 begin
 
-	addr_num := to_integer(unsigned());
+	addr_num := to_integer(unsigned(read_addr));
+
+	maze_out <- Mazes(addr_num);
 	
 end architecture;
--- timer.vhd
+++ timer.vhd
@@ -8,10 +8,37 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+-- Going to need some Arith library I cant remember right now
 
 entity timer is 
+    generic(
+        Default_Timer : integer := 6000;
+    );
+    port(
+        Clock, Reset : in std_logic;
+        output       : out std_logic_vector(13 downto 0); -- Can hold 9999
+    );
 end entity;
 
 architecture timer_arch of timer is
+
+    signal timer_value : integer;
+    signal clock_100Hz : std_logic;
+
 begin
+
+    -- Do some math to determine how much CLOCK_50 should be divided to get 1 hundreth of a second frequency
+    -- Then use that to govern the following process:
+
+    COUNTDOWN : process (clock_100Hz, Reset)
+    begin
+        if (Reset = '0') then
+            timer_value := Default_Timer;
+        elsif (rising_edge(clock_100Hz)) then
+            timer_value := timer_value - 1;
+        end if; 
+    end process;
+
+    output <= CONV_STD_LOGIC_VECTOR(timer_value, 14);
+
 end architecture;
