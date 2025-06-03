470c5381a8752ba01762613a9fb802059e6d5a64
BouvinGithub <155662148+BouvinGithub@users.noreply.github.com>
2025-06-03 12:20:56 -0400

    Update gameController.vhd

--- gameController.vhd
+++ gameController.vhd
@@ -1,1 +1,17 @@
+------------------------------------------------------------------
+-- ECE 318: Final Project - Maze Game
+-- Grant Bouvin, Collin Harrington, & Hoang Tran
+-- gameController: contains the logic of the ball position and goal checking
+-- Date: 6/3/2025
+------------------------------------------------------------------
 
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity gameContoller is 
+end entity;
+
+architecture gameController_arch of gameController is
+begin
+end architecture;
