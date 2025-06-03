2f55756f3fab1756c99c5d45098c80eb32627bb7
BouvinGithub <155662148+BouvinGithub@users.noreply.github.com>
2025-06-03 12:17:59 -0400

    Update timer.vhd

--- timer.vhd
+++ timer.vhd
@@ -1,1 +1,17 @@
+------------------------------------------------------------------
+-- ECE 318: Final Project - Maze Game
+-- Grant Bouvin, Collin Harrington, & Hoang Tran
+-- timer: controls the countdown timer that tells the player how much time they have left
+-- Date: 6/3/2025
+------------------------------------------------------------------
 
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity timer is 
+end entity;
+
+architecture timer_arch of timer is
+begin
+end architecture;
