237b4882f23c21590dd9c352b63836f352b2d629
Collin Harrington <collin1234harr@gmail.com>
2025-06-03 23:41:33 -0400

    Prelim gameDisplay completion

--- gameDisplay.vhd
+++ gameDisplay.vhd
@@ -11,9 +11,9 @@ use IEEE.std_logic_1164.all;
 entity gameDisplay is
 
     port(
-        pixel_row, pixel_column : std_logic_vector(9 downto 0); --Current position VGA is drawing
+        pixel_row, pixel_column : std_logic_vector(9 downto 0);  --Current position VGA is drawing
         maze_in                 : std_logic_vector(75 downto 0);
-        red, green, blue        : out std_logic --How to color the current pixel
+        red, green, blue        : out std_logic                  --How to color the current pixel
     );
 end entity;
 
@@ -21,11 +21,32 @@ architecture gameDisplay_arch of gameDisplay is
 begin
 
     DISPLAY_RGB : process --Sensitivity List???
+        signal indexX, indexY : integer;
+        signal indexPosition, ballPosition, goalPosition  : std_logic_vector(5 downto 0);
     begin
-        --Convert the pixel_row and pixel_column values to grid pos
-        --This may be doable with simple truncation
+        -- Convert the pixel row and column into index positions
+        indexX := to_integer(unsigned(pixel_row))    * 8 / 480 -- 8 for grid size 480 for number of rows. Note that divisible numbers are best
+        indexY := to_integer(unsigned(pixel_column)) * 8 / 640 -- 8 for grid size 480 for number of rows. Note that divisible numbers are best
 
-        --check the contents of the corresponding space in maze
-        --set RGB based on those contents
+        -- indexPosition matches the format of ballPosition and goalPosition from stored mazes
+        indexPosition := CONV_STD_LOGIC_VECTOR(indexX, 3) & CONV_STD_LOGIC_VECTOR(indexY, 3);
+
+        goalPosition := maze_in(75 downto 70);
+        ballPosition := maze_in(69 downto 64);
+
+        -- set the color of the pixel based on what is at the current location
+        if (indexPosition = goalPosition) then
+            red <= '1'; green <= '1'; blue <= '0'; --Yellow
+        elsif (indexPosition = ballPosition) then
+            red <= '0'; green <= '0'; blue <= '1'; --Blue
+        else
+
+            if ( maze_in(indexY * 8 + indexX) = '1') then
+                red <= '0'; green <= '0'; blue <= '0'; --Black
+            else
+                red <= '1'; green <= '1'; blue <= '1'; --White
+            end if;
+        end if;
     end process;
+
 end architecture;
