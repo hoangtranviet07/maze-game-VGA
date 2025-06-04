2d715e19a32c8af0484a3ecd943e153e66fd5d26
Collin Harrington <collin1234harr@gmail.com>
2025-06-03 22:19:54 -0400

    Fixed bad comment format

--- gameController.vhd
+++ gameController.vhd
@@ -12,8 +12,9 @@ use IEEE.numeric_std.all;
 entity gameContoller is 
 
     port (
+        Reset     : in std_logic;
         vert_sync : in std_logic;
-        up, down, left, right : in std_logic; //These should all be active low I believe based on how KEY0 works
+        up, down, left, right : in std_logic; --These should all be active low I believe based on how KEY0 works
         timer_in  : in std_logic_vector(13 downto 0);
         maze_in   : in std_logic_vector(75 downto 0);
         maze_addr : out std_logic_vector(1 downto 0);
@@ -23,22 +24,24 @@ end entity;
 
 architecture gameController_arch of gameController is
 
-    //When first loaded, ball position should be overwritten with the loaded mazes start positiion? A little clunk as is for now
+    --When first loaded, ball position should be overwritten with the loaded mazes start positiion? A little clunk as is for now
     signal ball_position : std_logic_vector(5 downto 0);
 begin
 
-    //Loading mazes???
+    --Will need button state tracking (pressed, held, released differences matter)
 
-    MOVE_BALL : process (vert_sync) //Is anything else needed here?
+    --Loading mazes???
+
+    MOVE_BALL : process (vert_sync) --Is anything else needed here?
     begin
-        //Check for up, down, left, right inputs
-        //Check if the desired grid position (current ball position + movement) is occupied
-        //If it is, dont do anything
-        //otherwise change ball position and
-        //check if ball position == goal position
-        //if so start the maze transition process. . . <-- Indicates state might be useful
+        --Check for up, down, left, right inputs
+        --Check if the desired grid position (current ball position + movement) is occupied
+        --If it is, dont do anything
+        --otherwise change ball position and
+        --check if ball position == goal position
+        --if so start the maze transition process. . . <-- Indicates state might be useful
     end process;
 
-    //Replace the start position with the current ball position
+    --Replace the start position with the current ball position
     maze_out <= maze_in(75 downto 70) & ball_position & maze_in(63 downto 0);
 end architecture;
--- gameDisplay.vhd
+++ gameDisplay.vhd
@@ -11,21 +11,21 @@ use IEEE.std_logic_1164.all;
 entity gameDisplay is
 
     port(
-        pixel_row, pixel_column : std_logic_vector(9 downto 0); //Current position VGA is drawing
+        pixel_row, pixel_column : std_logic_vector(9 downto 0); --Current position VGA is drawing
         maze_in                 : std_logic_vector(75 downto 0);
-        red, green, blue        : out std_logic //How to color the current pixel
+        red, green, blue        : out std_logic --How to color the current pixel
     );
 end entity;
 
 architecture gameDisplay_arch of gameDisplay is 
 begin
 
-    DISPLAY_RGB : process //Sensitivity List???
+    DISPLAY_RGB : process --Sensitivity List???
     begin
-        //Convert the pixel_row and pixel_column values to grid pos
-        //This may be doable with simple truncation
+        --Convert the pixel_row and pixel_column values to grid pos
+        --This may be doable with simple truncation
 
-        //check the contents of the corresponding space in maze
-        //set RGB based on those contents
+        --check the contents of the corresponding space in maze
+        --set RGB based on those contents
     end process;
 end architecture;
